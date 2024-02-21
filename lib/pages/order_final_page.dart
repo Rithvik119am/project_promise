1f93a1cc97db54dc29dfdd5f61d2008d9c01fd31
Rithvik119am <rithvikreddy524@gmail.com>
2024-02-21 18:30:22 +0530

    fixed the order view page

--- lib/pages/order_final_page.dart
+++ lib/pages/order_final_page.dart
@@ -13,63 +13,64 @@ class OrderPage extends StatelessWidget {
         title: const Text('Order Details'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Customer Details:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Text('Name: ${order.customer.name}'),
-            Text('Mobile Number: ${order.customer.mobileNumber}'),
-            Text('Address: ${order.customer.address}'),
-            const SizedBox(height: 20),
-            const Text(
-              'Granite Order Details:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Text('Number of Granite Types: ${order.numberOfGraniteTypes}'),
-            if (order.graniteOrders != null && order.graniteOrders!.isNotEmpty)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: order.graniteOrders!.map((graniteOrder) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 10),
-                      Text('Granite Name: ${graniteOrder.name}'),
-                      Text('Number of Slabs: ${graniteOrder.numberOfSlabs}'),
-                      Text('Square Feet: ${graniteOrder.squareFeet}'),
-                      Text('Per Square Feet: ${graniteOrder.perSqurare}'),
-                      Text('Per Granate price: ${graniteOrder.price}'),
-                    ],
-                  );
-                }).toList(),
-              ),
-            Expanded(
-              child: Center(
-                child: Text(
-                  'Value: ₹ ${order.calculatTotalAmount()}',
-                  style: const TextStyle(fontSize: 24),
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Customer Details:',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-              ),
+                const SizedBox(height: 10),
+                Text('Name: ${order.customer.name}'),
+                Text('Mobile Number: ${order.customer.mobileNumber}'),
+                Text('Address: ${order.customer.address}'),
+                const SizedBox(height: 20),
+                const Text(
+                  'Granite Order Details:',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                Text('Number of Granite Types: ${order.numberOfGraniteTypes}'),
+                if (order.graniteOrders != null &&
+                    order.graniteOrders!.isNotEmpty)
+                  SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: (order.graniteOrders ?? []).map((graniteOrder) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 10),
+                            Text('Granite Name: ${graniteOrder.name}'),
+                            Text(
+                                'Number of Slabs: ${graniteOrder.numberOfSlabs}'),
+                            Text('Square Feet: ${graniteOrder.squareFeet}'),
+                            Text('Per Square Feet: ${graniteOrder.perSqurare}'),
+                            Text('Per Granite price: ${graniteOrder.price}'),
+                          ],
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                Center(
+                  child: Text(
+                    'Value: ₹ ${order.calculatTotalAmount()}',
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(16),
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Next'),
+                  ),
+                ),
+              ],
             ),
-            Container(
-              margin: const EdgeInsets.all(16),
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Button action
-                },
-                child: const Text('Next'),
-              ),
-            ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
