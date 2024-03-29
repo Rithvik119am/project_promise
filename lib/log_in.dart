46aac11821212fd7fa117eaf0a2a3c4a713c32f4
munindra289 <munindrareddy289@gmail.com>
2024-03-29 21:40:32 +0530

    hi

--- lib/log_in.dart
+++ lib/log_in.dart
@@ -1,12 +1,173 @@
-import 'package:appwrite/appwrite.dart';
-import 'package:project_promise/groups/authenticate.dart';
 import 'package:flutter/material.dart';
+import 'package:project_promise/groups/authenticate.dart';
 import 'package:provider/provider.dart';
+import 'package:appwrite/appwrite.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class OutlineButtonFb1 extends StatelessWidget {
+  final String text;
+  final Function() onPressed;
+  final List<Color> gradientColors; // Add gradientColors parameter
+
+  const OutlineButtonFb1({
+    required this.text,
+    required this.onPressed,
+    required this.gradientColors, // Initialize gradientColors
+    Key? key,
+  }) : super(key: key);
 
   @override
+  Widget build(BuildContext context) {
+    const double borderRadius = 10;
+
+    return OutlinedButton(
+      onPressed: onPressed,
+      style: ButtonStyle(
+        side: MaterialStateProperty.all(BorderSide.none),
+        padding: MaterialStateProperty.all(
+          const EdgeInsets.symmetric(vertical: 20, horizontal: 1),
+        ),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+        ),
+      ),
+      child: ShaderMask(
+        blendMode: BlendMode.srcIn,
+        shaderCallback: (Rect bounds) {
+          return LinearGradient(
+            colors: gradientColors, // Use gradientColors
+          ).createShader(bounds);
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const Icon(Icons.arrow_forward, size: 30)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EmailInputFieldFb3 extends StatelessWidget {
+  final TextEditingController inputController;
+  const EmailInputFieldFb3({super.key, required this.inputController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: Colors.white,
+      ),
+      child: TextField(
+        controller: inputController,
+        onChanged: (value) {
+          // Do something with the value
+          value = inputController.text;
+        },
+        keyboardType: TextInputType.emailAddress,
+        style: const TextStyle(fontSize: 18, color: Colors.black, height: 1.5),
+        decoration: InputDecoration(
+          prefixIcon: ShaderMask(
+            blendMode: BlendMode.srcIn,
+            shaderCallback: (Rect bounds) {
+              return const LinearGradient(
+                colors: [Colors.blue, Colors.purple],
+              ).createShader(bounds);
+            },
+            child: const Icon(Icons.email, size: 30),
+          ),
+          filled: true,
+          hintText: 'Enter your email',
+          hintStyle: TextStyle(color: Colors.grey.withOpacity(.75)),
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 0.0, horizontal: 10.0),
+          border: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue, width: 1.0),
+            borderRadius: BorderRadius.all(Radius.circular(5.0)),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PasswordInputFieldFb3 extends StatefulWidget {
+  final TextEditingController inputController;
+  const PasswordInputFieldFb3({super.key, required this.inputController});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _PasswordInputFieldFb3State createState() => _PasswordInputFieldFb3State();
+}
+
+class _PasswordInputFieldFb3State extends State<PasswordInputFieldFb3> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: Colors.white,
+      ),
+      child: TextField(
+        controller: widget.inputController,
+        onChanged: (value) {
+          // Do something with the value
+          value = widget.inputController.text;
+        },
+        keyboardType: TextInputType.text,
+        style: const TextStyle(fontSize: 18, color: Colors.black, height: 1.5),
+        obscureText: _obscureText,
+        obscuringCharacter: '*',
+        decoration: InputDecoration(
+          prefixIcon: ShaderMask(
+            blendMode: BlendMode.srcIn,
+            shaderCallback: (Rect bounds) {
+              return const LinearGradient(
+                colors: [Colors.blue, Colors.purple],
+              ).createShader(bounds);
+            },
+            child: const Icon(Icons.lock, size: 30),
+          ),
+          suffixIcon: IconButton(
+            onPressed: () {
+              setState(() {
+                _obscureText = !_obscureText;
+              });
+            },
+            icon: Icon(_obscureText ? Icons.visibility_off : Icons.visibility),
+          ),
+          filled: true,
+          hintText: '********',
+          hintStyle: TextStyle(color: Colors.grey.withOpacity(.75)),
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 0.0, horizontal: 10.0),
+          border: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue, width: 1.0),
+            borderRadius: BorderRadius.all(Radius.circular(5.0)),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
   _LoginPageState createState() => _LoginPageState();
 }
 
@@ -17,18 +178,20 @@ class _LoginPageState extends State<LoginPage> {
 
   signIn() async {
     showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return const Dialog(
-            backgroundColor: Colors.transparent,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  CircularProgressIndicator(),
-                ]),
-          );
-        });
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return const Dialog(
+          backgroundColor: Colors.transparent,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CircularProgressIndicator(),
+            ],
+          ),
+        );
+      },
+    );
 
     try {
       final AuthAPI appwrite = context.read<AuthAPI>();
@@ -36,74 +199,90 @@ class _LoginPageState extends State<LoginPage> {
         email: emailTextController.text,
         password: passwordTextController.text,
       );
+      // ignore: use_build_context_synchronously
       Navigator.pop(context);
     } on AppwriteException catch (e) {
+      // ignore: use_build_context_synchronously
       Navigator.pop(context);
+      //print(e.message);
       showAlert(title: 'Login failed', text: e.message.toString());
     }
   }
 
   showAlert({required String title, required String text}) {
     showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text(title),
-            content: Text(text),
-            actions: [
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text('Ok'))
-            ],
-          );
-        });
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(text),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Ok'),
+            )
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('Appwrite App'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(''),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextField(
-                controller: emailTextController,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+                'assets/2.png'), // Add your background image path here
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 8.0),
+                  child: EmailInputFieldFb3(
+                      inputController:
+                          emailTextController), // Use EmailInputFieldFb3
                 ),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: passwordTextController,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 8.0),
+                  child: PasswordInputFieldFb3(
+                      inputController:
+                          passwordTextController), // Use PasswordInputFieldFb3
                 ),
-                obscureText: true,
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(double.infinity, 50),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 28.0, horizontal: 8.0),
+                  child: OutlineButtonFb1(
+                    text: 'Sign in',
+                    onPressed: signIn,
+                    gradientColors: const [
+                      Colors.blue,
+                      Colors.purple
+                    ], // Add gradient colors
+                  ),
                 ),
-                onPressed: () {
-                  signIn();
-                },
-                icon: const Icon(Icons.login),
-                label: const Text("Sign in"),
-              
-              ),
-              const SizedBox(height: 16),
-            ],
+                const SizedBox(height: 30),
+              ],
+            ),
           ),
         ),
       ),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:project_promise/pages/take_order/take_order.dart';
 import 'package:project_promise/pages/view_order/view_order.dart';
 import 'package:project_promise/pages/measurement_sheet/measurement_taker.dart';
@@ -6,90 +7,130 @@ import 'package:project_promise/groups/authenticate.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true, // Extend the body behind the app bar
       appBar: AppBar(
-        title: const Text('Home Page',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 255, 255, 255),
-            )),
+        backgroundColor: Colors.transparent, // Make app bar transparent
+        elevation: 0, // Remove app bar shadow
+        title: const Text(
+          '',
+        ),
         centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 255, 0, 0),
         leading: IconButton(
-          icon: const Icon(Icons.logout),
+          padding: const EdgeInsets.only(left: 30, top: 20),
+          hoverColor: Colors.transparent,
+          icon: const Icon(
+            Icons.logout,
+            color: Color.fromARGB(255, 0, 0, 0),
+            size: 40,
+          ),
           onPressed: () {
             final AuthAPI appwrite = context.read<AuthAPI>();
             appwrite.signOut();
           },
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => TakeOrderPage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.circular(10), // Adjust border radius here
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/2.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: kToolbarHeight + 20, // Adjust top padding for app bar
+                  bottom: 29.0, // Add padding here to move buttons up
                 ),
-                minimumSize: const Size(400, 100), // Adjust button size here
-              ),
-              child: const Text('Take Order',
-                  style: TextStyle(
-                      fontSize: 30, color: Color.fromARGB(255, 255, 0, 0))),
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ViewOrderPage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.circular(10), // Adjust border radius here
+                child: Column(
+                  children: [
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => TakeOrderPage(),
+                          ),
+                        );
+                      },
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide.none,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        minimumSize: const Size(320, 170),
+                      ),
+                      child: const Text(
+                        'Order',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Color.fromARGB(255, 255, 0, 0),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const ViewOrderPage(),
+                          ),
+                        );
+                      },
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide.none,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        minimumSize: const Size(320, 170),
+                      ),
+                      child: const Text(
+                        'History',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Color.fromARGB(255, 255, 0, 0),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const MeasurementSheet(),
+                          ),
+                        );
+                      },
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide.none,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        minimumSize: const Size(320, 170),
+                      ),
+                      child: const Text(
+                        'Measurement Sheet',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Color.fromARGB(255, 255, 0, 0),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                minimumSize: const Size(400, 100), // Adjust button size here
               ),
-              child: const Text('View Order',
-                  style: TextStyle(
-                      fontSize: 30, color: Color.fromARGB(255, 255, 0, 0))),
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const MeasurementSheet()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.circular(10), // Adjust border radius here
-                ),
-                minimumSize: const Size(400, 100), // Adjust button size here
-              ),
-              child: const Text('Measurement Sheet',
-                  style: TextStyle(
-                      fontSize: 30, color: Color.fromARGB(255, 255, 0, 0))),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
